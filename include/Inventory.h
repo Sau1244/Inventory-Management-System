a67e0a5ebff1f0d7ce19e83e4b319c81c04448fb
Sau1244 <regec.hubert@gmail.com>
2026-03-25 10:56:34 +0100

    Implemented read and write csv functions

--- include/Inventory.h
+++ include/Inventory.h
@@ -22,8 +22,14 @@ public:
     std::vector<std::shared_ptr<Item>>& getStock();
     const std::vector<std::shared_ptr<Item>>& getStock() const;
 
+    void readFromFile(const std::string& filename);
+    void writeToFile(const std::string& filename) const;
+
     void addItem(std::shared_ptr<Item> newItem);
     void removeItem(const std::string& itemID);
     void updateQuantity(const std::string& itemID, int quantity);
     void displayInventory() const;
+private:
+    static std::vector<std::string> splitString(const std::string& line);
+    void addItemFromFile(const std::vector<std::string>& columns);
 };
--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -1,11 +1,60 @@
 #include "Inventory.h"
 #include "Item.h"
 #include "Exceptions.h"
+#include "Factory.h"
 
 #include <algorithm>
+#include <sstream>
+#include <fstream>
 
 std::vector<std::shared_ptr<Item>>& Inventory::getStock() { return stock; }
 const std::vector<std::shared_ptr<Item>>& Inventory::getStock() const { return stock; }
+
+
+void Inventory::readFromFile(const std::string& filename){
+    if(!stock.empty())
+        throw InventoryException("Stock is not empty.");
+
+    std::ifstream file(filename);
+
+    if(!file.is_open())
+        throw InventoryException("Unable to open " + filename);
+
+    std::string line;
+
+    while(std::getline(file, line)){
+        auto columns = splitString(line);
+        try {
+            addItemFromFile(columns);
+        }catch(const InventoryException& e) {
+            std::cout << "\n[Error] " << e.what() << "\n";
+        }
+    }
+
+    file.close();
+}
+
+void Inventory::writeToFile(const std::string& filename) const{
+    if(stock.empty())
+        throw InventoryException("Stock is empty.");
+
+    std::ofstream file(filename);
+
+    if(!file.is_open())
+        throw InventoryException("Unable to create " + filename);
+
+    for(const auto& i : stock){
+        auto category = i->category();
+
+        auto extra = category == "Electronics" ? i->findAttribute("warranty") : i->findAttribute("expiration");
+        if(!extra)
+            throw InventoryException("Extra Column is empty");
+
+        file << i->getItemID() << ',' << i->category() << ','
+            << i->getName() << ',' << i->getQuantity() << ',' << i->getPrice() << ','
+            << extra.value() << "\n";
+    }
+}
 
 void Inventory::addItem(std::shared_ptr<Item> newItem) {
     bool isExist = std::any_of(stock.begin(), stock.end(),
@@ -40,4 +89,40 @@ void Inventory::updateQuantity(const std::string &itemID, int quantity) {
 void Inventory::displayInventory() const {
     for(const auto& i : stock)
         i->display();
+}
+
+std::vector<std::string> Inventory::splitString(const std::string& line){
+    std::vector<std::string> result;
+    std::stringstream ss(line);
+    std::string column;
+
+    while(std::getline(ss, column, ',')){
+        result.push_back(column);
+    }
+
+    return result;
+}
+
+void Inventory::addItemFromFile(const std::vector<std::string>& columns){
+    if(columns.size() < 6)
+        throw InventoryException("Invalid row format.");
+
+    const std::string& itemID = columns[0];
+    const std::string& category = columns[1];
+    const std::string& name = columns[2];
+
+    try {
+        int quantity = std::stoi(columns[3]);
+        double price = std::stod(columns[4]);
+
+        if(category == "Electronics")
+            addItem(Factory::makeElectronics(itemID, name, quantity, price, std::stoi(columns[5])));
+        else if(category == "Grocery")
+            addItem(Factory::makeGrocery(itemID, name, quantity, price, columns[5]));
+        else
+            throw InventoryException("Category " + category + " does not exist.");
+
+    }catch(const std::exception&){
+        throw InventoryException("Invalid number format.");
+    }
 }
